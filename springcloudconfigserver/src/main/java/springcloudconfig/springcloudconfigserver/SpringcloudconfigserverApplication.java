07744d74330f5a19d46f0ae96537ae3bf89bf06f
kindomdjw <34157189+kindomdjw@users.noreply.github.com>
2019-04-28 09:51:27 +0800

    create a new project

---
+++ springcloudconfigserver/src/main/java/springcloudconfig/springcloudconfigserver/SpringcloudconfigserverApplication.java
@@ -0,0 +1,18 @@
+package springcloudconfig.springcloudconfigserver;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableConfigServer
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringcloudconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudconfigserverApplication.class, args);
+	}
+
+}
